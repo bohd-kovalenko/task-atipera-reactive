649d53f8bac2a7d3af0eb98f9b01cca7e2d2fafb
CoolTrol <bohdan1234yunior@gmail.com>
2023-08-27 16:51:31 +0300

    Initial commit

---
+++ src/main/java/com/atipera/testtaskreactive/TestTaskReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.atipera.testtaskreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atipera/testtaskreactive/TestTaskReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.testtaskreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
